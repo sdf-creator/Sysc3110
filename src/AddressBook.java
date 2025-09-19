966745ccbdaefdeca0a25c7c84f61c0e136587e2
Aditi Nahar <aditinahar2005@gmail.com>
2025-09-18 23:02:28 -0400

    test

---
+++ src/AddressBook.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private ArrayList <BuddyInfo> buddies;
+
+    public AddressBook() {
+        this.buddies = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo buddy) {
+        if (buddy != null) {
+            buddies.add(buddy);
+        }
+    }
+
+    public void removeBuddy(BuddyInfo buddy) {
+        buddies.remove(buddy);
+
+    }
+
+    public static void main(String[] args){
+        System.out.println("Address Book");
+    }
+}
